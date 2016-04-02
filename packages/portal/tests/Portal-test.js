f38333a0544a005a6007bf7cd8a3faa57ee6742b
Brian Link <cpsubrian@gmail.com>
2016-04-02 16:00:55 -0700

    Initial commit /with Portal component.

---
+++ packages/portal/tests/Portal-test.js
@@ -0,0 +1,22 @@
+import expect from 'expect'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Portal from 'src/Portal'
+
+describe('Component', () => {
+  let node
+
+  beforeEach(() => {
+    node = document.createElement('div')
+  })
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(node)
+  })
+
+  it('body contains portal content', () => {
+    ReactDOM.render(<Component/>, node, () => {
+      expect(node.innerHTML).toContain('Welcome to React components')
+    })
+  })
+})

c392030fce9fe547ac7295e715dcb1f55dd8e439
Brian Link <cpsubrian@gmail.com>
2017-05-17 16:56:08 -0700

    Move mapbox stories to a folder.

--- modules/react-mapbox/src/stories/basic.story.js
+++ modules/react-mapbox/src/stories/basic.story.js
@@ -1,24 +1,9 @@
 import React from 'react'
-import {MapGL, Layer} from './'
-
-const defaults = {
-  accessToken: 'pk.eyJ1IjoidGVycmEiLCJhIjoiVmNta3lMSSJ9.V4vST11PV1hulV2Mf9DqdQ',
-  style: 'mapbox://styles/mapbox/streets-v9',
-  bbox: [[-123.881836, 25.063209], [-65.170898, 48.848451]],
-  center: [-95.844727, 39.620499],
-  zoom: [3],
-  padding: 30,
-  containerStyle: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
-  }
-}
+import {MapGL, Layer} from '../'
+import defaults from './defaults'
 
 export default function ({storiesOf, action}) {
-  storiesOf('Mapbox GL', module).add('Basic Map', () => {
+  storiesOf('Mapbox', module).add('Basic Map', () => {
     return (
       <MapGL {...defaults}>
         <Layer
